2d13a1a90c8ad75a68532d4c60b3208c7ea050f8
Aniket Ray <139179800+Synaptic-rayan@users.noreply.github.com>
2023-11-18 01:54:47 +0530

    Update ReverseInteger.cpp

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int reverse(int x) {
+        long long res = 0;
+        long long ans = 0;
+         while(x!=0){
+            ans = x %10;
+             x/=10;
+          res = res*10+ans;
+          }
+          if(res > INT_MAX || res < INT_MIN) return 0;
+        return res;
+    }
+};
