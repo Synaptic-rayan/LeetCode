b7ffa93c863b98f5c75ba7914a8f6b4b540c6173
Aniket Ray <139179800+Synaptic-rayan@users.noreply.github.com>
2023-11-18 01:47:27 +0530

    Create 3Sum.cpp

---
+++ 3Sum.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+       vector<vector<int>> threeSum(std::vector<int>& nums) {
+        vector<std::vector<int>> result;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 2; ++i) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int left = i + 1, right = n - 1, target = -nums[i];
+
+                while (left < right) {
+                    int sum = nums[left] + nums[right];
+
+                    if (sum == target) {
+                        result.push_back({nums[i], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+};
